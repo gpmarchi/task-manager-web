dfc16dac97dd7a1041b1b4e109c7d45ddd02b6b8
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-12-09 18:58:44 -0300

    add task list component

---
+++ src/components/TaskList/index.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
+
+import "./styles.css";
+
+export default function TaskList({ selectedProject }) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function loadTasks() {
+      const token = localStorage.getItem("token");
+      try {
+        const response = await api.get(
+          `/tasks?project=${selectedProject.projectId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }
+        );
+        setTasks(response.data);
+      } catch (error) {
+        console.log(error.response);
+      }
+    }
+    loadTasks();
+  }, [selectedProject]);
+
+  return (
+    <div className="task-list">
+      <h1>{selectedProject.projectName}</h1>
+      <ul className="main-projects">
+        {tasks.map(task => {
+          return (
+            <li className="task-item" key={task._id}>
+              {task.description}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
